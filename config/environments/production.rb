595ca3956ee249334434070a5a065ec47e8225a8
Michael Siegfried <michaeldsiegfried@gmail.com>
2022-08-11 18:43:50 -0700

    Address new rubocop rules

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
     address:              'smtp.sendgrid.net',
     port:                 '587',
     authentication:       :plain,
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
+    user_name:            ENV.fetch('SENDGRID_USERNAME'),
+    password:             ENV.fetch('SENDGRID_PASSWORD'),
     domain:               'heroku.com',
     enable_starttls_auto: true
   }
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+  provider :google_oauth2, ENV.fetch('GOOGLE_CLIENT_ID', nil), ENV.fetch('GOOGLE_CLIENT_SECRET', nil), {
     access_type: :offline,
     name:        :google,
     prompt:      :select_account
