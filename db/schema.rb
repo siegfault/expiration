f63c06c081c76efb40fc72fc6f543f780fefa5ef
Michael Siegfried <michaeldsiegfried@gmail.com>
2022-08-11 19:15:10 -0700

    Address missing unique constraint
    Since we have a validation on the uniqueness of email, we should also
    have a matching db constraint for that.
    
    <3 Rails/UniqueValidationWithoutIndex

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_195537) do
+ActiveRecord::Schema.define(version: 2022_08_12_020856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_05_18_195537) do
     t.text "token"
     t.text "refresh_token"
     t.datetime "oauth_expires_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
