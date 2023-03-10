1a8af37ab781d86149fe56e80705b8cf42e22faa
Michael Siegfried <michaeldsiegfried@gmail.com>
2023-03-09 19:57:40 -0800

    Fix Style/Documentation
    Document base class and move logic that should've been in the base class
    into it.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
+# Base mailer for other mailers to inherit behavior from
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'notifications@expiration.xyz'
   layout 'mailer'
 end
